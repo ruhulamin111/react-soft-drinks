2c674798ebeb9af1d8c99d07d826068536fbf14a
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-08-24 15:05:32 +0600

    random item update

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -6,15 +6,23 @@ import Item from './Item/Item';
 const Home = () => {
     const [drinks, setDrinks] = useState([]);
     const [cart, setCart] = useState([]);
+    const [random, setRandom] = useState({});
 
     useEffect(() => {
         fetch('https://www.thecocktaildb.com/api/json/v1/1/search.php?s=vodka   ')
             .then(res => res.json())
             .then(data => setDrinks(data.drinks))
     }, [drinks]);
+
     const addToCart = (selected) => {
         const newCart = [...cart, selected];
         setCart(newCart);
+    }
+
+    const handleRandom = () => {
+        const randomItem = Math.floor(Math.random() * cart.length);
+        const item = cart[randomItem];
+        setRandom(item);
     }
 
     return (
@@ -35,9 +43,16 @@ const Home = () => {
                         cart.map((item, index) => <Item
                             key={index}
                             item={item}
+                            random={random}
                         ></Item>)
                     }
-                    <button>Order Now</button>
+                    <button onClick={handleRandom}>Order Now</button>
+                    {<div className='item'>
+                        <img className='item-img' src={random.strDrinkThumb} alt="" />
+                        <p>{random.strDrink}</p>
+                    </div>
+
+                    }
                 </div>
             </dir>
         </div>
