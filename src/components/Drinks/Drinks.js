04f1bef970022a54f9251515ca54bccf3af0a4a7
Ruhul Juwel <ruhuljuwel111@gmail.com>
2022-08-24 09:33:23 +0600

    components add

---
+++ src/components/Drinks/Drinks.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Drinks.css'
+
+const Drinks = () => {
+    return (
+        <div>
+            <h2>Drinks</h2>
+        </div>
+    );
+};
+
+export default Drinks;
